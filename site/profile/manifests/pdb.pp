f8fe3c154b344b1636630fa71b54d941f945de11
Brian Snyder <bsnyder74@gmail.com>
2016-07-22 11:09:02 -0700

    update puppetdb class

--- site/profile/manifests/pdb.pp
+++ site/profile/manifests/pdb.pp
@@ -3,24 +3,27 @@ class profile::pdb {
   class { 'puppetdb::database::postgresql':
     listen_addresses => '*'
   }
+  #
+  # Running into this bug with the firewalld module
+  # https://github.com/crayfishx/puppet-firewalld/pull/46
 
-  firewalld_rich_rule {'5432 postgresql':
-    ensure   => present,
-    zone     => 'public',
-    port     => {
-      'port'     => 5432,
-      'protocol' => 'tcp',
-    },
-    action   => 'accept',
-  }
-
-  firewalld_rich_rule {'8081 puppetdb':
-    ensure   => present,
-    zone     => 'public',
-    port     => {
-      'port'     => 80801,
-      'protocol' => 'tcp',
-    },
-    action   => 'accept',
-  }
+  # firewalld_rich_rule {'5432 postgresql':
+  #   ensure   => present,
+  #   zone     => 'public',
+  #   port     => {
+  #     'port'     => 5432,
+  #     'protocol' => 'tcp',
+  #   },
+  #   action   => 'accept',
+  # }
+  #
+  # firewalld_rich_rule {'8081 puppetdb':
+  #   ensure   => present,
+  #   zone     => 'public',
+  #   port     => {
+  #     'port'     => 80801,
+  #     'protocol' => 'tcp',
+  #   },
+  #   action   => 'accept',
+  # }
 }
